a8eac663e950195a56e3427c9079f334d9c43405
iittaayyy13579@gmail.com <talsagi3012@gmail.com>
2025-09-04 23:37:27 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/') 
+def hello_world(): 
     return 'Hello, CI/CD with Jenkins, Docker, and Kubernetes!'
 
 if __name__ == '__main__':
